9e209874846efba01ff96d428aecec694069d027
EminCimen <46209839+EminCimen@users.noreply.github.com>
2019-10-08 14:42:17 +0300

    Add files via upload

---
+++ metot/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int sonuc;
+        static int Hesapla(int a, int b)
+        {
+            int x, y;
+            x = 3 * a;
+            y = b / 2;
+            return (x + y);
+
+        }
+    
+        static void Main(string[] args)
+        {
+            int n;
+            n = Hesapla(4, 8);
+            sonuc = n;
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
